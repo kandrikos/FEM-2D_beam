a676703cc9225b2b75c225f3f8e6d421e76f2e70
Kostas Andrikos <120322455+kandrikos@users.noreply.github.com>
2023-03-13 00:50:25 +0200

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -5,6 +5,6 @@ shared_args = {'n_elemX': 40,       # number of elements along x-axis
                'poisson': 0.3,      # poisson ratio
                'thickness': 0.2}    # thickness of beam (m)
 
-determ_moduli = 1e5   # modulus of elasticity (kN/m2)
+determ_modulus = 1e5   # modulus of elasticity (kN/m2)
 determ_load = 10      # applied load (kN)
 n_simulations = 100   # number of simulations 
